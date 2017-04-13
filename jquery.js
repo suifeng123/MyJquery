53c11470134736c24716f5599aa06916e24954f9
wangshengwen <wswen1220@163.com>
2017-04-13 15:02:45 +0800

    MyJquery

--- jquery.js
+++ jquery.js
@@ -195,7 +195,148 @@
                 throw  msg;
 
             },
-            
+            //解析json
+            parseJSON: function(data){
+                if(typeof  data !== "string" || !data){
+                    return null;
+                }
+                data = jQuery.trim(data);
+
+                if(window.JSON && window.JSON.parse){
+                    return window.JSON.parse(data);
+                }
+                //检查一下字符串的合法性
+                if(rvalidchars.test(data.replace(rvalidchars,"@").replace(rvalidchars,"]")
+                        .replace(rvalidchars,""))){
+                    return (new  Function("return"+data))();
+                }
+                jQuery.error("无效的json 格式："+data);
+            },
+           //跨浏览器解析xml
+            parseXML: function (data,xml,tmp) {
+
+                //非ie
+                if(window.DOMParser){
+                    tmp = new DOMParser();
+                    xml = tmp.parseFromString(data,"text/xml");
+                }else {
+                    xml = new ActiveXObject("Microsoft.XMLDOM");//微软定义的
+                    xml.async = "false";
+                    xml.loadXML(data);
+                }
+                tmp = xml.documentElement;
+                if(!tmp || !tmp.nodeName || tmp.nodeName==="parsererror"){
+                    jQuery.error("无效的xml:"+data);
+                }
+                return xml;
+
+            },
+            //无操作函数
+           noop: function () {
+
+           },
+
+            globalEval: function (data) {
+                //data 非空
+                if(data && rnotwhite.test(data)){
+                    (window.execScript||function (data) {
+                        window["eval"].call(window,data);
+
+                    })(data);
+                }
+
+            },
+            //判断节点名称是否相同
+            nodeName: function (elem,name) {
+                return elem.nodeName && elem.nodeName.toUpperCase()===name.toUpperCase();
+
+            },
+            //遍历数组对象
+            each: function (object,callback,args) {
+                var name,i = 0;
+                var length = object.length;
+                var isObj = length === undefined || jQuery.isFunction(object);
+                //如果参数有args,调用apply,
+                if(args){
+                    if(isObj){
+                        for(name in object){
+                            if(callback.apply(object[name],args)===false){
+                                break;
+                            }
+                        }
+                    }else {
+                        for(;i<length;){
+                            if(callback.apply(object[i++],args)===false){
+                                break;
+                            }
+                        }
+                    }
+                }else {
+                    if(isObj){
+                        for(name in object){
+                            if(callback.call(object[name],name,object[name])===false){
+                                break;
+                            }
+                        }
+                    }else {
+                        for(;i<length;){
+                            if(callback.call(object[i],i,object[i++])===false){
+                                break;
+                            }
+                        }
+                    }
+                }
+                return object;
+
+            },
+
+            trim:trim?function (text) {
+                return text==null?"":trim.call(text);
+
+                }: function (text) {
+                    return text==null?"":text.toString().replace(trimLeft,"").replace(trimRight,"");
+
+                },
+
+            //将假的数组妆花为数组
+            makeArray: function (array,results) {
+                    var ret = results||[];
+                    if(array!=null){
+                        var type= jQuery.type(array);
+                        if(array.length== null || type==="string"
+                        || type==="function"||type==="regexp"
+                        ||jQuery.isWindow(array)){
+                            push.call(ret,array);
+                        }else {
+                            jQuery.merge(ret,array);
+                        }
+                    }
+                    return ret;
+
+            },
+
+           //判断是否在数组中‘
+            inArray: function (elem,array) {
+                //是否函数本地化的Array.prototype.indexOf
+                if(indexOf){
+                    return indexOf.call(array,elem);
+                }
+                for(var i=0,length=array.length;i<length;i++){
+                    if(array[i]===elem){
+                        return i;
+                    }
+                }
+                return -1;
+            },
+
+            //将数组second合并到first中
+            merge:function (first,second) {
+                    var i = first.length;
+                    var j=0;
+                    
+
+            }
+
 
 
 
