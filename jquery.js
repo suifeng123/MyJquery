acaea6235b9fabeb326aab8145ac51a9ece33131
wangshengwen <wswen1220@163.com>
2017-04-13 14:24:41 +0800

    MyJquery

--- jquery.js
+++ jquery.js
@@ -76,7 +76,129 @@
               return jQuery;
             },
 
-            isReaady: false,
+            isReady: false,
+
+            readyWait: 1,
+
+            holdReady: function (hold) {
+                if(hold){
+                    jQuery.readyWait++;
+                }else {
+                    jQuery.ready(true);
+                }
+
+            },
+            //文档加载完毕
+
+            ready: function (wait) {
+
+                if((wait === true && !--jQuery.readyWait) || (wait !==true && !jQuery.isReady)){
+                    //确保document存在
+                    if(!document.body){
+                        return setTimeout(jQuery.ready,1);
+                    }
+
+                    jQuery.isReady = true;
+
+                    if(wait !== true && --jQuery.readyWait>0){
+                        return;
+                    }
+
+                    readyList.resolveWith(document,[jQuery]);
+
+                    if(jQuery.fn.trigger){
+                        jQuery(document).trigger("ready").unbind("ready");
+                    }
+                }
+
+            },
+
+            //初始化readyList 的事件队列
+            bindReady: function () {
+                if(readyList){
+                    return;
+                }
+
+                readyList = jQuery._Deffered();
+
+                if(document.readyState === "complete"){
+                    return setTimeout(jQuery.ready,1);
+                }
+                //兼容事件
+                if(document.addEventListener){
+                    document.addEventListener("DOMContentLoaded",DOMContentLoaded,false);
+
+                    window.addEventListener("load",jQuery.ready,false);
+                }else if(document.attachEvent){
+                    document.attachEvent("onreadystatechange",DOMContentLoaded);
+                    window.attachEvent("onload",jQuery.ready);
+                    var toplevel = false;
+
+                    try{
+                        toplevel = window.frameElement == null
+                    }catch (e){
+
+                    }
+                    if(document.documentElement.doScroll && toplevel){
+                        doScrollCheck();
+                    }
+                }
+            },
+
+            isFunction: function (obj) {
+
+                return jQuery.type(obj) === "function";
+
+            },
+
+            //是否师叔祖
+            isArray: Array.isArray || function (obj) {
+                return jQuery.type(obj) === "array";
+
+            },
+
+            isWindow: function (obj) {
+                return obj && typeof obj === "object" && "setInterval" in obj;
+
+            },
+
+            isNaN: function(obj){
+                return obj == null || !rdigit.test(obj) || isNaN(obj);
+        },
+            type: function (obj) {
+                return obj==null?String(obj):class2type[toString.call(obj)] || "object";
+
+            },
+            isPlainObject: function (obj) {
+                if(!obj || jQuery.type(obj)!=="object" || obj.nodeType||jQuery.isWindow(obj)){
+                    return false;
+                }
+                if(obj.constructor && !hasOwn.call(obj,"constructor")&&!hasOwn.call(obj.constructor.prototype,"isPrototypeOf")){
+                    return false;
+                }
+
+                var key;
+                for(key in obj){}
+
+                return key === undefined || hasOwn.call(obj,key);
+
+            },
+            //判断是否为空对象
+            isEmptyObject: function (obj) {
+                for(var name in obj){
+                    return false;
+                }
+                return true;
+
+            },
+            error: function (msg) {
+                throw  msg;
+
+            },
+            
+
+
+
 
         });
         return jQuery;
